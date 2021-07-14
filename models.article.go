04f5a52b712ec12f4acb207bbb4039c0fecf7073
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2021-07-14 06:30:26 -0400

    Complete the whole codebase.

--- models.article.go
+++ models.article.go
@@ -1,4 +1,8 @@
+// models.article.go
+
 package main
+
+import "errors"
 
 type article struct {
 	ID      int    `json:"id"`
@@ -6,11 +10,36 @@ type article struct {
 	Content string `json:"content"`
 }
 
+// For this demo, we're storing the article list in memory
+// In a real application, this list will most likely be fetched
+// from a database or from static files
 var articleList = []article{
-	article{1, "Title 1", "Article 1 body"},
-	article{2, "Title 2", "Article 2 body"},
+	article{ID: 1, Title: "Article 1", Content: "Article 1 body"},
+	article{ID: 2, Title: "Article 2", Content: "Article 2 body"},
 }
 
+// Return a list of all the articles
 func getAllArticles() []article {
 	return articleList
+}
+
+// Fetch an article based on the ID supplied
+func getArticleByID(id int) (*article, error) {
+	for _, a := range articleList {
+		if a.ID == id {
+			return &a, nil
+		}
+	}
+	return nil, errors.New("Article not found")
+}
+
+// Create a new article with the title and content provided
+func createNewArticle(title, content string) (*article, error) {
+	// Set the ID of a new article to one more than the number of articles
+	a := article{ID: len(articleList) + 1, Title: title, Content: content}
+
+	// Add the article to the list of articles
+	articleList = append(articleList, a)
+
+	return &a, nil
 }
